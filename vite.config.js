6181f454c65da5830583e2cd6fcd16c2372aa593
Anibal <androso421@gmail.com>
2022-01-04 13:35:21 -0600

    adding instructions for avoiding refreshing the page every x seconds when outside of replit

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,21 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 /**
  * https://vitejs.dev/config/
  * @type { import('vite').UserConfig }
- */
+*/
+
+
+//! For use on Replit only
+// export default {
+//   plugins: [reactRefresh()],
+//   server: {
+//     host: '0.0.0.0',
+//     hmr: {
+//       port: 443,
+//     }
+//   }
+// }
+
+//! IF YOU'RE NOT USING REPLIT USE THIS CONFIG INSTEAD 
 export default {
-  plugins: [reactRefresh()],
-  server: {
-    host: '0.0.0.0',
-    hmr: {
-      port: 443,
-    }
-  }
+  plugins: [reactRefresh()]
 }
